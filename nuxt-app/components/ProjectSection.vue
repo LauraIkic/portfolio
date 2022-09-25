f4973aad4ae1fd3e6cca87a3ace8cae2900981c0
Laura Ikic <ikic.laura@gmx.at>
2022-09-25 19:59:16 +0200

    Test

--- nuxt-app/components/ProjectSection.vue
+++ nuxt-app/components/ProjectSection.vue
@@ -17,16 +17,16 @@
       </li>
     </ul>
 
-    <div class="flex justify-center mt-20">
+<!--    <div class="flex justify-center mt-20">
       <a href="projects" class="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group">
         <span class="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
-        <!-- Top glass gradient -->
+        &lt;!&ndash; Top glass gradient &ndash;&gt;
         <span class="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
-        <!-- Bottom gradient -->
+        &lt;!&ndash; Bottom gradient &ndash;&gt;
         <span class="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
-        <!-- Left gradient -->
+        &lt;!&ndash; Left gradient &ndash;&gt;
         <span class="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
-        <!-- Right gradient -->
+        &lt;!&ndash; Right gradient &ndash;&gt;
         <span class="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
         <span class="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
         <span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
@@ -34,7 +34,7 @@
         Read more
       </span>
       </a>
-    </div>
+    </div>-->
 
   </div>
 
