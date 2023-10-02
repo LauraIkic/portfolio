86d4a7b9ad0d131c832268d4af9790d023da21d6
Laura Ikic <lauraikic@Lauras-MacBook-Pro.local>
2023-10-02 12:46:50 +0200

    Fix image display

--- nuxt-app/components/ProjectPreview.vue
+++ nuxt-app/components/ProjectPreview.vue
@@ -3,13 +3,11 @@
     <div class="relative group w-80">
       <div class="absolute -inset-1 bg-gradient-to-r from-[#0096c9] to-[#a21faf] rounded-2xl blur opacity-25 group-hover:opacity-100 transition duration-1000 group-hover:duration-200"></div>
       <div class="relative h-56 bg-white ring-1 ring-gray-900/5 rounded-2xl leading-none flex items-top justify-start bg-[#474e55cf]">
-        <img :src="project.image"  width="350px" height="150px" alt=""
+        <img v-bind:src="project.image" width="350px" height="150px" alt=""
              class="absolute  object-cover cursor-pointer pointer-events-none transition duration-500 shadow-md rounded-t-2xl grayscale group-hover:grayscale-0 absolute top-0"
         />
-
         <p class="text-white flex flex-col justify-end p-4">
           {{project.title}}
-          {{ project.image }}
         </p>
       </div>
     </div>
