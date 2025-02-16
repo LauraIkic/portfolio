d131f25ad253ece63011ba7e221d28887487aa4d
lauraikic <laura.ikic@massiveart.com>
2025-02-16 15:15:55 +0100

    Add experiences and small image size fixes.

--- src/app/components/Item.tsx
+++ src/app/components/Item.tsx
@@ -13,6 +13,7 @@ interface ProjectProps {
         name: string;
         title: string;
         image: string;
+        date: string;
         description: string;
         icon: string;
         tools: string[];
@@ -21,8 +22,7 @@ interface ProjectProps {
 }
 
 const Item = ({ project }: ProjectProps) => {
-    const { image, title, description, tools } = project;
-
+    const { image, title,date ,  description, tools } = project;
     return (
         <article className="flex flex-col md:flex-row group m-4 border border-[#DFDFDF] hover:border hover:border-secondary p-4 transition-all duration-300">
             {project.image !== '' ? (
@@ -36,12 +36,13 @@ const Item = ({ project }: ProjectProps) => {
                         className="object-cover transition-all sm:w-[350px] md:max-w-[250px] duration-300 transform group-hover:scale-105 grayscale-[0.8] group-hover:grayscale-0"
                     />
                 </div>)
-             :null}
+             :null }
 
 
             <div className="flex flex-col mt-4 md:mt-0 md:ml-10">
                 <header className="mb-4">
                     <h3 className="text-xl font-semibold group-hover:text-secondary">{title}</h3>
+                    {date ? ( <p className="text-sm sm:text-bas">{date}</p>) : null}
                     <p className="text-sm sm:text-base">{description}</p>
                 </header>
                 <section className="flex flex-wrap gap-2 h-1/3">
