fd0ddd919f5072cd586c9baff8d17a47d7d3c580
lauraikic <laura.ikic@massiveart.com>
2025-02-16 15:30:59 +0100

    Add fixes.

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,10 +1,13 @@
 import experienceData from './experience.json';
 import {Item} from '../components/Item';
+import Link from "next/link";
 export default function Page() {
     return (
         <div className="w-1/2 mx-auto">
             {experienceData.map((reference) => (
-                <Item key={reference.name} project={reference} />
+                <Link href={reference.externalLink} key={reference.name} target="_blank">
+                    <Item key={reference.name} project={reference} />
+                </Link>
             ))}
         </div>
     );
