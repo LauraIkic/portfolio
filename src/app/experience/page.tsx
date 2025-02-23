a78bb20cff281eb733dd7d774b959f1dd0a53f91
lauraikic <ikic.laura@gmx.at>
2025-02-23 21:40:57 +0100

    Update experience data.

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,11 +1,12 @@
 import experienceData from './experience.json';
 import {Item} from '../components/Item';
 import Link from "next/link";
+
 export default function Page() {
     return (
         <div className="lg:w-1/2 sm-w-full mx-auto">
             {experienceData.map((category) => (
-                <div>
+                <div key={category.name}>
                     <h2 className="pl-4 pt-8 text-xl font-bold text-secondary">{category.title}</h2>
                     {
                         category.items.map((reference)=>
