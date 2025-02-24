ebff4f8e2b1989339816dca346ed3699e5ca09a8
lauraikic <ikic.laura@gmx.at>
2025-02-24 19:57:33 +0100

    Fix eslint interation key error.

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     return (
         <div className="lg:w-1/2 sm-w-full mx-auto">
             {experienceData.map((category) => (
-                <div>
+                <div key={category}>
                     <h2 className="pl-4 pt-8 text-xl font-bold text-secondary">{category.title}</h2>
                     {
                         category.items.map((reference)=>
