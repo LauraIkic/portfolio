106310a38a82066126160a26fe8ed625234ee080
lauraikic <laura.ikic@massiveart.com>
2025-03-06 15:17:18 +0100

    Improve accordion item.

--- src/app/components/AccordionItem.tsx
+++ src/app/components/AccordionItem.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import { FaPlus, FaMinus } from 'react-icons/fa';
-import Marker from './Marker';
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 
 interface LinkProps {
     title: string;
@@ -34,37 +33,36 @@ const AccordionItem = ({ project }: ProjectProps) => {
                 className="cursor-pointer flex justify-between items-center"
                 onClick={toggleAccordion}
             >
-                <h3 className="md:text-xl font-semibold">{title}</h3>
+                <h3 className="md:text-xl">{title}</h3>
                 <span
-                    className={`w-8 h-8 flex items-center justify-center rounded-full transition-transform duration-300 
-                        ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+                    className={`w-8 h-8 flex items-center justify-center rounded-full 
+                        transition-all duration-300 transform ${
+                        isOpen ? 'rotate-180 scale-110' : 'rotate-0 scale-100'
+                    }`}
                 >
-                    {isOpen ? (
-                        <FaMinus className="text-lg" />
-                    ) : (
-                        <FaPlus className="text-lg" />
-                    )}
+                    {isOpen ? <SlArrowUp /> : <SlArrowDown />}
                 </span>
             </div>
-
-            {isOpen && (
-                <div className="flex flex-col">
-                    <div className="flex flex-col">
-                        <header className="mb-4 mt-2 pt-2 border-t">
-                            {date && <p className="text-gray-600">{date}</p>}
-                            <p className="text-gray-700">{description}</p>
-                        </header>
-                        <section className="flex flex-wrap gap-2 ">
-                                {tools.map((tool, index) => (
-                                    <Marker key={index} marker={tool}/>
-                                ))}
-                        </section>
-                    </div>
-
+            <div
+                className={`transition-all duration-500 ease-in-out overflow-hidden 
+                    ${isOpen ? 'max-h-[1000px] opacity-100' : 'max-h-0 opacity-0'}`}
+            >
+                <div className="flex flex-col mt-4">
+                    <header className="mb-4 mt-2 pt-2 border-t">
+                        {date && <p className="text-gray-600">{date}</p>}
+                        <p className="text-gray-700">{description}</p>
+                    </header>
+                    <section className="flex flex-wrap gap-2">
+                        {tools.map((tool, index) => (
+                            <div key={index} className="bg-gray-200 px-4 py-1 rounded-full text-sm">
+                                {tool}
+                            </div>
+                        ))}
+                    </section>
                 </div>
-            )}
+            </div>
         </article>
     );
 };
 
-export {AccordionItem};
+export { AccordionItem };
