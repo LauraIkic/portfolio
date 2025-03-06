3ff510793d127375900515bbded7b95744d93293
lauraikic <laura.ikic@massiveart.com>
2025-03-06 15:26:37 +0100

    Fake loading projects with spinner.

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,15 +1,33 @@
 'use client';
+import { useState, useEffect } from 'react';
 import projectsData from './projects.json';
-import {Item} from '../components/Item';
+import { Item } from '../components/Item';
 import Link from "next/link";
+
 export default function Page() {
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 100);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
         <div className="lg:w-1/2 sm-w-full mx-auto">
-            {projectsData.map((projectItem) => (
-                <Link href={`/projects/${projectItem.name}`} key={projectItem.name}>
-                <Item key={projectItem.name} project={projectItem} />
-                </Link>
-            ))}
+            {isLoading ? (
+                <div className="flex justify-center items-center h-48">
+                    <div className="w-10 h-10 border-2 border-t-transparent border-solid rounded-full animate-spin"></div>
+                </div>
+            ) : (
+                projectsData.map((projectItem) => (
+                    <Link href={`/projects/${projectItem.name}`} key={projectItem.name}>
+                        <Item key={projectItem.name} project={projectItem} />
+                    </Link>
+                ))
+            )}
         </div>
     );
 }
